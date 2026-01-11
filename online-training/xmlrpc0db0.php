b4938dffa268386f6b9e3424a8b76a28686efb90
Satnam Singh <singhkhowal@gmail.com>
2026-01-11 08:15:18 +0530

    'maps'

--- online-training/xmlrpc0db0.php
+++ online-training/xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://caddeskindia.com/online-training/</homePageLink>
+		<homePageLink>https://CadMateIndia.com/online-training/</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://caddeskindia.com/online-training/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://caddeskindia.com/online-training/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://caddeskindia.com/online-training/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://caddeskindia.com/online-training/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://caddeskindia.com/online-training/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://CadMateIndia.com/online-training/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://CadMateIndia.com/online-training/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://CadMateIndia.com/online-training/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://CadMateIndia.com/online-training/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://CadMateIndia.com/online-training/wp-json/" />
 			</apis>
 	</service>
 </rsd>
